3e36ad3e3a48babae8a34746b56c9b93e8d77aa1
emphasize <swen_g@t-online.de>
2022-01-28 13:53:39 +0100

    fix snapshots (files as list)

--- doorpi/status/status_lib/history_snapshot.py
+++ doorpi/status/status_lib/history_snapshot.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Iterable, List
+from typing import Iterable, List
 
 import doorpi.actions.snapshot
 import doorpi.doorpi
@@ -11,16 +11,13 @@ def get(
 ) -> List[str]:
     del doorpi_obj, name, value
 
-    path = str(doorpi.actions.snapshot.SnapshotAction.get_full_path())
-    files: Iterable[str] = map(
-        str, doorpi.actions.snapshot.SnapshotAction.list_all()
-    )
+    #path = str(doorpi.actions.snapshot.SnapshotAction.get_full_path())
     # because path is added by webserver automatically
     # TODO think this is a fragment and not needed any more
-    if "DoorPiWeb" in path:
-        changedpath = path[path.find("DoorPiWeb") + len("DoorPiWeb"):]
-        files = [f.replace(path, changedpath) for f in files]
-    return files
+    # if "DoorPiWeb" in path:
+    #     changedpath = path[path.find("DoorPiWeb") + len("DoorPiWeb"):]
+    #     files = [f.replace(path, changedpath) for f in files]
+    return list(map(str, doorpi.actions.snapshot.SnapshotAction.list_all()))
 
 def is_active(doorpi_obj: doorpi.doorpi.DoorPi) -> bool:
     del doorpi_obj
