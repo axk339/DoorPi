2e1a31f2023af32cda6e4663c7ceb00da25e3b52
RXH <admim@rxh>
2025-09-23 21:56:53 +0200

    akuvox action

--- doorpi/actions/akuvox.py
+++ doorpi/actions/akuvox.py
@@ -8,45 +8,187 @@ from doorpi.status.status_class import DoorPiStatus
 from doorpi.actions import snapshot
 
 import datetime
+import doorpi
 
 from . import Action
 
 LOGGER = logging.getLogger(__name__)
 
-#/home/pi/DoorPi/DoorPi.egg-info/entry_points.txt
+import requests
+
+def akuvoxDND (force, dnd_mute):
+	getdnd = True		# get always status
+	if force:
+		setdnd = True	# and update if target is different
+	else:
+		setdnd = False
+	
+	getvol = True		# always get volume
+	setvol = True		# and set to 10 if different (except 0, which forces setdnd to mute)
+	vol_level = 10
+	
+	debug = False
+	
+	indfile = doorpi.INSTANCE.config["akuvox.indfile"]
+	akuvoxPasswordHash = doorpi.INSTANCE.config["akuvox.pwhash"]
+	url = 'http://' + doorpi.INSTANCE.config["akuvox.ip"] + '/web'
+	try:
+		### manage login ###
+		
+		postdata = '{"target":"login","action":"set","data":{"password":"' + akuvoxPasswordHash + '"},"session":"","web":"1"}'
+		response = requests.post(url, data=postdata)
+		response.raise_for_status() # This will raise an HTTPError for bad responses (4xx or 5xx)
+		
+		if debug:
+			print (f"3) post password - response: {response.text}")
+		
+		start_index = response.text.find("encrypt") + 11
+		end_index = start_index + 32
+		encrypt = response.text[start_index:end_index]
+		
+		if debug:
+			print  (f"# received encrypt: {encrypt}")
+		
+		postdata = '{"target":"login","action":"login","data":{"userName":"admin","password":"' + encrypt + '"},"session":"","web":"1"}'
+		response = requests.post(url, data=postdata)
+		response.raise_for_status() # This will raise an HTTPError for bad responses (4xx or 5xx)
+		
+		if debug:
+			print (f"4) post login - response: {response.text}")
+		
+		start_index = response.text.find("token") + 9
+		end_index = start_index + 8
+		token = response.text[start_index:end_index]
+		
+		if debug:
+			print  (f"# received session token: {token}")
+		
+		### get dnd status - store in local file for further processing
+		
+		if getdnd:
+			postdata = '{"target":"config","action":"info","configData":{"item":["WholeDay&24&61"]},"session":"' + token + '","web":"1"}';
+			response = requests.post(url, data=postdata)
+			response.raise_for_status() # This will raise an HTTPError for bad responses (4xx or 5xx)
+			
+			if debug:
+				print (f"11) get dnd - response: {response.text}")
+			
+			## parse dnd status (..."WholeDay": "1"...)
+			start_index = response.text.find("WholeDay") + 12
+			end_index = start_index + 1
+			dndstatus = response.text[start_index:end_index]
+			
+			if dndstatus == "0":
+				if setdnd and not dnd_mute:
+					setdnd = False
+					print ("# skipping force unmute, not needed")
+				with open(indfile, "w") as f:
+					f.write("unmute")
+				print ("# stored status 'unmute' in " + str(indfile))
+			else:
+				if setdnd and dnd_mute:
+					setdnd = False
+					print ("# skipping force mute, not needed")
+				with open(indfile, "w") as f:
+					f.write("mute")
+				print ("# stored status 'mute' in " + str(indfile))
+		
+		### set dnd status, both in local file and on ip phone
+		
+		if setdnd:
+			postdata = '{"target":"config","action":"edit","configData":{"item":["' + ('1' if dnd_mute else '0') + '&24&61"]},"session":"' + token + '","web":"1"}'
+			response = requests.post(url, data=postdata)
+			response.raise_for_status() # This will raise an HTTPError for bad responses (4xx or 5xx)
+			
+			if debug:
+				print (f"12) set dnd - response: {response.text}")
+			
+			if dnd_mute:
+				with open(indfile, "w") as f:
+					f.write("mute")
+				print ("# stored status 'mute' in " + str(indfile))
+			else:
+				with open(indfile, "w") as f:
+					f.write("unmute")
+				print ("# stored status 'unmute' in " + str(indfile))
+			
+			print ("# new dnd status: " + ('1' if dnd_mute else '0'))
+		
+		### get current ring volume
+		
+		if getvol:
+			
+			#{"target":"config","action":"info","configData":{"item":["RingVolume&26&266","TalkVolume&26&272","MicVolume&26&273","TouchSoundEnable&26&1561","ringtoneSoundFile&26&224","SelectRingtoneSoundFile&26&2432"]},"session":"E4D37207","web":"1"}
+			postdata = '{"target":"config","action":"info","configData":{"item":["RingVolume&26&266"]},"session":"' + token + '","web":"1"}'
+			response = requests.post(url, data=postdata)
+			response.raise_for_status() # This will raise an HTTPError for bad responses (4xx or 5xx)
+			
+			if debug:
+				print (f"7a) get ring tone volume - response: {response.text}")
+			
+			# parse dnd status (..."RingVolume": "10"...)
+			start_index = response.text.find("RingVolume") + 14
+			end_index = start_index + 2
+			volstatus = int(response.text[start_index:end_index])
+			
+			if setvol and vol_level == volstatus:
+				setvol = False
+				print ("# skipping force volume, not needed")
+			
+			print ("# volume: " + str(volstatus))
+		
+		### set volume to defined value (0..15, by default always 10)
+		
+		if setvol:
+			
+			if vol_level < 0: vol_level = 0
+			if vol_level > 15: vol_level = 15
+			
+			postdata = '{"target":"config","action":"edit","configData":{"item":["' + str(vol_level) + '&26&266"]},"session":"' + token + '","web":"1"}'
+			response = requests.post(url, data=postdata)
+			response.raise_for_status() # This will raise an HTTPError for bad responses (4xx or 5xx)
+			
+			if debug:
+				print (f"7) set ring tone volume - response: {response.text}")
+			
+			print ("# new volume: " + str(vol_level))
+		
+		
+	except requests.exceptions.RequestException as e:
+		print (f"An error occurred: {e}")
+
 
 class AkuvoxAction(Action):
     """Gets Akuvox status or sends update."""
 
-    def __init__(self, filename: str, *content: str) -> None:
+    def __init__(self, action: str) -> None:
         super().__init__()
         
-        self.__content = ",".join(content).strip()
-        self.__logpath = filename
-        self.nextFile()
+        if action == "setmute":
+            self.__force = True
+            self.__dnd_mute = True
+        elif action == "setunmute":
+            self.__force = True
+            self.__dnd_mute = False
+        else:
+            self.__force = False
+            self.__dnd_mute = False
     
-    def nextFile(self):
-        self.__filename = pathlib.Path(doorpi.INSTANCE.parse_string(self.__logpath) + "/doorpi_events.txt")
-        
-        self.__filename.parent.mkdir(parents=True, exist_ok=True)
-
     def __call__(self, event_id: str, extra: Mapping[str, Any]) -> None:
-        content = doorpi.INSTANCE.parse_string(self.__content)
-        
         try:
-            self.nextFile()
-            with self.__filename.open("a") as f:
-                f.write(content+",1\n")
+            akuvoxDND (self.__force, self.__dnd_mute)
         except Exception:  # pylint: disable=broad-except
             LOGGER.exception(
-                "[%s] Error fetching status information for file %s",
+                "[%s] Error contacting akuvox monitor: %s",
                 event_id,
                 self.__filename,
             )
-        
 
     def __str__(self) -> str:
-        return f"Write current status into {self.__filename}"
+        if self.__force:
+            return f"Force Akuvox mute={self.__dnd_mute}"
+        else:
+            return f"Get Akuvox mute status"
 
     def __repr__(self) -> str:
-        return f"statusfile:{self.__filename},{self.__content.strip()}"
+        return f"akuvox:{self.__force},{self.__dnd_mute}"
